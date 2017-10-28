3f5ab6f7050bb77276456a2bf213b12535c85178
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2017-10-29 06:11:36 +0700

    Connect API and Fix format

--- src/constrain.js
+++ src/constrain.js
@@ -1,4 +1,4 @@
-export const JobList = ['iOS Developer', 'Android Developer', 'Frontend Developer', 'Backend Developer', 'FullStack Developer', 'Data Engineer', 'Data Scientist'];
+export const JobList = ['iOS Developer', 'Android Developer', 'Frontend Developer', 'Backend Developer', 'Full-Stack Developer', 'Data Engineer', 'Data Scientist'];
 export const PostJobURI = ``;
 
 export const fadeInterval = 100;
